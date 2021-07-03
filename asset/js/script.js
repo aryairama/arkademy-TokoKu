e2e22a9cd0f9dc5d6d53cc9a5bfeca5533174eda
Arya Irama Wahono <aryairama987@gmail.com>
2021-07-03 17:36:04 +0700

    page seller myProducts

---
+++ asset/js/script.js
@@ -0,0 +1,99 @@
+import "./jquery.min.js";
+import "./owl.carousel.min.js";
+// Carousel
+$("#carousel-trend").owlCarousel({
+  nav: true,
+  center: true,
+  items: 3,
+  loop: true,
+  margin: 30,
+  dotsClass: "owl-dots text-start",
+  navContainerClass: "owl-nav position-relative",
+  navClass: [
+    "owl-prev shadow carousel-trend-btn-prev",
+    "owl-next shadow carousel-trend-btn-next",
+  ],
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1.5,
+    },
+    576: {
+      items: 1.5,
+    },
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 2.5,
+    },
+  },
+});
+
+$("#carousel-category").owlCarousel({
+  nav: true,
+  dots: false,
+  loop: true,
+  margin: 30,
+  dotsClass: "owl-dots text-start",
+  navContainerClass: "owl-nav position-relative",
+  navClass: [
+    "owl-prev shadow carousel-category-btn-prev",
+    "owl-next shadow carousel-category-btn-next",
+  ],
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    576: {
+      items: 2.4,
+    },
+    768: {
+      items: 3,
+    },
+    992: {
+      items: 4,
+    },
+    1200: {
+      items: 5,
+    },
+  },
+});
+// Carousel
+//sidebar
+$(document).on("click", ".sidebar-toggler", function () {
+  $(".sidebar").toggleClass("sidebar-active");
+  $(".main-panel").toggleClass("w-100");
+});
+// sidebar
+//modal add new address
+if ($("#add-new-address").length > 0) {
+  const modalAddNewAddress = new bootstrap.Modal($("#add-new-address"));
+  if ($("#choose-another-address").length > 0) {
+    const modalChooseAnotheAddress = new bootstrap.Modal($("#choose-another-address"));
+    $(".add-new-address").on("click", function () {
+      modalAddNewAddress.show();
+      modalChooseAnotheAddress.hide();
+    });
+  }
+  $(".add-new-address").on("click", function () {
+    modalAddNewAddress.show();
+  });
+}
+//modal add new address
+// Product gallery
+$(".small-product-gallery").on("click", ".img-small-product-gallery", function () {
+    let clickImg = this
+    let targetImg = $(".img-big-product-gallery");
+    targetImg.fadeOut("slow", () => {
+        $(".img-small-product-gallery-active").removeClass("img-small-product-gallery-active");
+        $(clickImg).addClass("img-small-product-gallery-active");
+        targetImg.attr("src", clickImg.getAttribute("src")).fadeIn("slow")
+    })
+});
+// Product gallery
